7c58f4d6ee095c15038cd1e1e95f14cb6364e295
Yaroslav Stefinko <ystefinko@intellias.com>
2019-08-05 18:13:20 +0300

    fix of boost
    Signed-off-by: Yaroslav Stefinko <ystefinko@intellias.com>

--- scripts/android/build.sh
+++ scripts/android/build.sh
@@ -4,4 +4,5 @@ mkdir -p build && cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE="$WORKSPACE/android-ndk-r20/build/cmake/android.toolchain.cmake"  -DANDROID_PLATFORM=android-28 -DANDROID_ABI=arm64-v8a -DEDGE_SDK_ENABLE_TESTING=NO -DEDGE_SDK_BUILD_EXAMPLES=ON
 sudo make install -j16
 cd examples/dataservice-read/android
-./gradlew assemble
+sudo dpkg -s libboost-dev | grep Version
+sudo ./gradlew assemble
