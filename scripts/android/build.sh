ef38a2d50e8ed6f78188b75122da29481354ff18
Yaroslav Stefinko <ystefinko@intellias.com>
2019-08-06 11:17:00 +0300

    Added boost root variable to cmake
    Signed-off-by: Yaroslav Stefinko <ystefinko@intellias.com>

--- scripts/android/build.sh
+++ scripts/android/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 mv android-9 android-9_source && mkdir -p android-9/platforms/android-28 && cp -r android-9_source/* android-9/platforms/android-28/ && rm -rf android-9_source
 mkdir -p build && cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE="$WORKSPACE/android-ndk-r20/build/cmake/android.toolchain.cmake" -DBOOST_ROOT="$WORKSPACE/build/external/boost/external_boost/boost"-DANDROID_PLATFORM=android-28 -DANDROID_ABI=arm64-v8a -DEDGE_SDK_ENABLE_TESTING=NO -DEDGE_SDK_BUILD_EXAMPLES=ON
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$WORKSPACE/android-ndk-r20/build/cmake/android.toolchain.cmake" -DBOOST_ROOT="$WORKSPACE/build/external/boost/external_boost/boost" -DANDROID_PLATFORM=android-28 -DANDROID_ABI=arm64-v8a -DEDGE_SDK_ENABLE_TESTING=NO -DEDGE_SDK_BUILD_EXAMPLES=ON
 sudo make install -j16
 cd examples/dataservice-read/android
 ( cat $WORKSPACE/build/external/boost/external_boost/boost/version.hpp | grep "BOOST_LIB_VERSION" )  || true
